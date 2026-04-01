f42dd0c3f03b00aa21713f2b65c3e412ab3d3b82
mayumi-josanin <josanin.mayumi@gmail.com>
2026-04-01 18:09:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ===== GAS設定 =====
 // ↓ GASウェブアプリURLをここに貼り付け ↓
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbw3Chs-C7wkDrRloEwZlwQQkaq0PIu3l5kZqSi-rR1omXQIGBYKldsKwVefRcKNsPKC4w/exec';
-const CURRENT_WEB_BUNDLE_VERSION = '2026.04.01.20';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwbi4IOPNlEVvcpAuqJ-uEjYD1AcshAZSfbc_8Z0ROUxuXinT_Ot5R87ggKuIySVoW1/exec';
+const CURRENT_WEB_BUNDLE_VERSION = '2026.04.01.21';
 const APP_RUNTIME_CONFIG_STORAGE_KEY = 'mayumi_app_runtime_config';
 const DEFAULT_APP_RUNTIME_CONFIG = Object.freeze({
   latestAppVersion: '1.1.0',
